ee991acdc6d25f36f502716f2b5d91a74ae07d82
markbarm <markbarm@gmail.com>
2021-03-05 20:44:56 -0300

    Programa recebe o valor de um dolar em reais e a quantos dolares serão comprados. O valor a ser pago incluirá o IOF de 6%.

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,19 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import util.CurrencyConversion;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Whats is the dollar price? ");
+		double dollarPrice = sc.nextDouble();
+		System.out.print("How many dollar will be bought? ");
+		double quantity = sc.nextDouble();
+		
+		System.out.printf("Amount to paid in reais %.2f%n", CurrencyConversion.payment(quantity, dollarPrice));
 		
 		sc.close();
 		
